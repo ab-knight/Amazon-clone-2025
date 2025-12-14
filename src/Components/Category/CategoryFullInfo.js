8f89b8e544fa678c3d117017c67b781171901e79
ab-knight <150024162+ab-knight@users.noreply.github.com>
2025-12-15 05:05:24 +0530

    Category

---
+++ src/Components/Category/CategoryFullInfo.js
@@ -0,0 +1,27 @@
+export const categoryInfos = [
+  {
+    title: "Electronics",
+    name: "electronics",
+    imgLink:
+      "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_TV_2X._SY304_CB432517900_.jpg",
+  },
+
+  {
+    title: "Discover Fashion trends",
+    name: "women's clothing",
+    imgLink:
+      "https://images.unsplash.com/photo-1521334884684-d80222895322?auto=format&fit=crop&w=800&q=80",
+  },
+  {
+    title: "Men's Clothing",
+    name: "men's clothing",
+    imgLink:
+      "https://images.unsplash.com/photo-1520975916090-3105956dac38?auto=format&fit=crop&w=800&q=80",
+  },
+  {
+    title: "Jewelery",
+    name: "jewelery",
+    imgLink:
+      "https://images.unsplash.com/photo-1512163143273-bde0e3cc7407?auto=format&fit=crop&w=800&q=80",
+  },
+];
