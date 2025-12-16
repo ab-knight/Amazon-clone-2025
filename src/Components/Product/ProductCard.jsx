e0a2ccbcd91c7d647439b4065155286fd0fe214a
ab-knight <150024162+ab-knight@users.noreply.github.com>
2025-12-16 10:19:07 +0530

    Detail page routing

--- src/Components/Product/ProductCard.jsx
+++ src/Components/Product/ProductCard.jsx
@@ -2,21 +2,23 @@ import React from "react";
 import Rating from "@mui/material/Rating";
 import CurrencyFormat from "../CurrencyFormat/CurrencyFormat";
 import classes from "./Product.module.css";
+import { Link } from "react-router-dom";
 
 function ProductCard({ product }) {
   const { image, title, id, rating, price } = product;
   return (
     <div className={classes.card_container}>
-      <a href="">
+      <Link to={`/products/${id}`}>
         <img src={image} alt="" />
-      </a>
+      </Link>
       <div>
         <h3>{title}</h3>
         <div className={classes.rating}>
           {/* rating */}
-          <Rating value={rating.rate} precision={0.1} />
+          <Rating value={Number(rating?.rate) || 0} precision={0.1} readOnly />
+
           {/* count */}
-          <small>{rating.count}</small>
+          <small>{rating?.count}</small>
         </div>
         <div>
           {/* price */}
--- src/Pages/ProductDetail/ProductDetail.jsx
+++ src/Pages/ProductDetail/ProductDetail.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import Layout from '../../Components/Layout/Layout';
+
+import React, { useEffect, useState } from "react";
+import Layout from "../../Components/Layout/Layout";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { productUrl } from "../../Api/endPoint";
+import ProductCard from '../../Components/Product/ProductCard';
 
 function ProductDetail() {
-  return (
-    <Layout>
-      <div>ProductDetail</div>
-    </Layout>
-  );
+  const { productId } = useParams();
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`${productUrl}/products/${productId}`)
+      .then((res) => {
+        setProduct(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+ return (
+   <Layout>
+     <ProductCard product={product} />
+   </Layout>
+ );
+
 }
 
-export default ProductDetail
+export default ProductDetail;
