b10e552f5b2aaf11e9cdcf0485f52439aec56261
ab-knight <150024162+ab-knight@users.noreply.github.com>
2025-12-20 14:10:03 +0530

    signout functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
 import Routing from './Router';
+import  { useContext, useEffect } from "react";
+import { DataContext } from "./Components/DataProvider/DataProvider.jsx";
+import { Type } from "./Utility/action.type.js";
+import { auth } from "./Utility/firebase.js";
 
 function App() {
+  const [{ user }, dispatch] = useContext(DataContext);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // console.log(authUser);
+        dispatch({
+          type: Type.SET_USER,
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: Type.SET_USER,
+          user: null,
+        });
+      }
+    });
+  }, []);
+
+
   return (
     <>
       <Routing />
--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import classes from "./SignUp.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
 import { auth } from "../../Utility/firebase";
 import {
   signInWithEmailAndPassword,
@@ -8,41 +8,57 @@ import {
 } from "firebase/auth";
 import { DataContext } from "../../Components/DataProvider/DataProvider";
 import { Type } from "../../Utility/action.type"
+import { ClipLoader } from "react-spinners";
 
 function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [{ user }, dispatch] = useContext(DataContext);
+  const [loading, setLoading] =useState({
+    signIn: false,
+    signUp: false
+  });
+  const navigate = useNavigate()
 
+
+  
   console.log(user);
 
   const authHandler = (e) => {
     e.preventDefault();
 
     if (e.target.name === "signin") {
+      setLoading({...loading, signIn:true})
       signInWithEmailAndPassword(auth, email, password)
         .then((userInfo) => {
           dispatch({
             type: Type.SET_USER,
             user: userInfo.user,
           });
+          setLoading({ ...loading, signIn: false });
+          navigate("/")
         })
         .catch((err) => {
           console.log(err);
           setError(err.message);
+          setLoading({ ...loading, signIn: false });
         });
     } else {
+      setLoading({ ...loading, signUp: true });
       createUserWithEmailAndPassword(auth, email, password)
         .then((userInfo) => {
           dispatch({
             type: Type.SET_USER,
             user: userInfo.user,
           });
+          setLoading({ ...loading, signUp: false });
+          navigate("/");
         })
         .catch((err) => {
           console.log(err);
           setError(err.message);
+          setLoading({ ...loading, signUp: false });
         });
     }
   };
@@ -84,7 +100,7 @@ function Auth() {
             onClick={authHandler}
             className={classes.login_signinButton}
           >
-            Sign In
+            {loading.signIn ? <ClipLoader color="#000" size={15} /> : "Sign In"}
           </button>
         </form>
 
@@ -93,8 +109,16 @@ function Auth() {
           onClick={authHandler}
           className={classes.login_registerButton}
         >
-          Create your Amazon Account
+          {" "}
+          {loading.signUp ? (
+            <ClipLoader color="#000" size={15} />
+          ) : (
+            "Create your Amazon Account"
+          )}
         </button>
+        {error && (
+          <small style={{ paddingTop: "5px", color: "red" }}>{error}</small>
+        )}
       </div>
     </section>
   );
